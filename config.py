de70c8a1534d74d238bbde30dfbb45c34c319fbb
Nenosaj <jasondaohog1@gmail.com>
2026-03-09 15:32:55 +0800

    feat: removed account

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 # --- IMAP SETTINGS (For Reading the Trigger Email) ---
-EMAIL_ACCOUNT = "daohog.jason1@gmail.com"        # Replace with your email
-APP_PASSWORD = "hzgd qcfy aoqk audi"  # Replace with your App Password
+EMAIL_ACCOUNT = ""        # Replace with your email
+APP_PASSWORD = ""  # Replace with your App Password
 IMAP_SERVER = "imap.gmail.com"
 TARGET_SUBJECT = "Fwd: CNFM OPEN FT and WO"
 
@@ -16,7 +16,7 @@ SENDER_PASSWORD = APP_PASSWORD                # Reusing the same App Password
 # Add the emails of everyone who needs this report. 
 # For testing, just put your own email here first!
 RECIPIENT_EMAILS = [
-    "cacao.thesis08@gmail.com", 
+    "", 
 
 ]
 
